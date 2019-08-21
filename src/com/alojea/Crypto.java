b3e6b6796fe4ab4da39ea2fea76e0d998d17cdd9
alojea <alojea@gmail.com>
2019-08-21 18:45:42 +0100

    Initial Commit

---
+++ src/com/alojea/Crypto.java
@@ -0,0 +1,140 @@
+package com.alojea;
+
+public class Crypto {
+
+    static final int MODULE = 26;
+
+    public String encrypt(String clearText, String keyStream) {
+        int[] clearTextConvertedInNumber = convertStringIntoNumber(clearText);
+        int[] keyStreamConvertedInNumber = convertStringIntoNumber(keyStream);
+        int[] textConverted = addCleartextWithKeystream(clearTextConvertedInNumber,keyStreamConvertedInNumber);
+        
+        return returnCipherText(textConverted);
+    }
+
+    public String decrypt(String cipherText, String keyStream) {
+        int[] cipherTextConvertedInNumber = convertStringIntoNumber(cipherText);
+        int[] keyStreamConvertedInNumber = convertStringIntoNumber(keyStream);
+        int[] textConverted = substractKeystreamWithCiphertext(cipherTextConvertedInNumber,keyStreamConvertedInNumber);
+
+        return returnCipherText(textConverted);
+    }
+
+    /**
+     * addCleartextWithKeystream
+     *
+     * Function for add Cleartext with keystream
+     * The result of add need to be less than the module 26
+     * if the number is > 26 subtract 26
+     *
+     * @return int[] with result of add cleartext and keystream
+     */
+    public int[] addCleartextWithKeystream(int[] cleartextConverted, int[] keystreamConverted){
+        int minLenght = 0;
+        int[] result = null;
+
+        try {
+            if(cleartextConverted.length >=  keystreamConverted.length){
+                minLenght = keystreamConverted.length;
+                result = cleartextConverted.clone();
+            } else {
+                minLenght = cleartextConverted.length;
+                result = keystreamConverted.clone();
+            }
+
+            for(int index=0;index<minLenght;index++){
+                result[index] = cleartextConverted[index] + keystreamConverted[index];
+                if(result[index] > MODULE){
+                    result[index] = result[index] - MODULE;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * substractKeystreamWithciphertext
+     *
+     * Substract keystream and Ciphertext
+     * if first number is less than second
+     * add module 26 to the first number before
+     * subtracting
+     *
+     * @return  int[] with result of subtract cipher and keystream
+     *
+     */
+    public int[] substractKeystreamWithCiphertext(int[] cipherTextConverted, int[] keyStreamConverted){
+        int minLenght = 0;
+        int[] result = null;
+
+        try {
+            if(cipherTextConverted.length >=  keyStreamConverted.length){
+                minLenght = keyStreamConverted.length;
+                result = cipherTextConverted.clone();
+            } else {
+                minLenght = cipherTextConverted.length;
+                result = keyStreamConverted.clone();
+            }
+
+            for(int index=0;index<minLenght;index++){
+
+                if(cipherTextConverted[index] < keyStreamConverted[index]){
+                    result[index] = ( cipherTextConverted[index] + MODULE ) - keyStreamConverted[index];
+                } else {
+                    result[index] = cipherTextConverted[index] - keyStreamConverted[index];
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+
+    }
+
+    /**
+     *
+     * convertNumberToString
+     *
+     *
+     * @param numberToConvert
+     * @return
+     */
+    public String returnCipherText(int[] numberToConvert){
+        char[] arrayChar = new char[numberToConvert.length];
+
+        try {
+            for(int index = 0 ; index < numberToConvert.length; index++){
+                arrayChar[index] = CryptoUtils.returnChar(numberToConvert[index]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return String.valueOf(arrayChar);
+    }
+
+    /**
+     * Convert String into Number
+     *
+     * @param textToConvert
+     * @return String converted in number
+     */
+    public int[] convertStringIntoNumber(String textToConvert){
+        int[] intArray = new int[textToConvert.length()];
+
+        try {
+            for(int i=0;i<textToConvert.length();i++){
+                intArray[i] = CryptoUtils.returnNumber(textToConvert.charAt(i));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return intArray;
+    }
+
+}
---
+++ src/com/alojea/CryptoTest.java
@@ -0,0 +1,56 @@
+package com.alojea;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CryptoTest{
+
+    Crypto crypto;
+
+    @Before
+    public void setUp() throws Exception {
+        crypto = new Crypto();
+    }
+
+    @Test
+    public void encryptTest(){
+        Assert.assertEquals(crypto.encrypt("DONOTUSEPC","KDWUPONOWT"),"OSKJJJGTMW");
+    }
+
+    @Test
+    public void decryptTest(){
+        Assert.assertEquals(crypto.decrypt("OSKJJJGTMW","KDWUPONOWT"),"DONOTUSEPC");
+    }
+
+    @Test
+    public void convertStringIntoNumberTest(){
+        int [] numberTest = {4,15,14,15,20,21,19,5,16,3};
+        Assert.assertArrayEquals(crypto.convertStringIntoNumber("DONOTUSEPC"),numberTest);
+    }
+
+    @Test
+    public void addCleartextWithKeystreamTest(){
+        int[] result = {15,19,11,10,10,10,7,20,13,23};
+        int[] cleartextConverted = {4,15,14,15,20,21,19,5,16,3};
+        int[] keystreamConverted = {11,4,23,21,16,15,14,15,23,20};
+        Assert.assertArrayEquals(crypto.addCleartextWithKeystream(cleartextConverted,keystreamConverted),result);
+    }
+
+    @Test
+    public void substractKeystreamWithCiphertextTest(){
+        int[] result = {4,15,14,15,20,21,19,5,16,3};
+        int[] ciphertext = {15,19,11,10,10,10,7,20,13,23};
+        int[] keystreamConverted = {11,4,23,21,16,15,14,15,23,20};
+        Assert.assertArrayEquals(crypto.substractKeystreamWithCiphertext(ciphertext,keystreamConverted),result);
+    }
+
+    @Test
+    public void returnCipherTextTest(){
+        int[] result = {4,15,14,15,20,21,19,5,16,3};
+        Assert.assertEquals(crypto.returnCipherText(result),"DONOTUSEPC");
+
+    }
+
+}
